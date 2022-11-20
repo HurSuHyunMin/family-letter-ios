0555dceb241908ea9593cb385c7edd30e46be714
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-11-20 18:28:25 +0900

    ✨ 탭바 구현 (#2)

---
+++ family-letter-ios/views/scenes/CalendarScene.swift
@@ -0,0 +1,23 @@
+//
+//  CalendarScene.swift
+//  family-letter-ios
+//
+//  Created by 우현민 on 2022/11/20.
+//
+
+import SwiftUI
+
+struct CalendarScene: View {
+    var body: some View {
+        Text("달력")
+    }
+}
+
+struct CalendarScene_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView {
+            CalendarScene()
+        }
+    }
+}
+
